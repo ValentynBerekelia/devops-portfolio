60778eba3da3bc5dbcd1c058ff919db479e8e6a0
ValentynBerekelia <126330340+ValentynBerekelia@users.noreply.github.com>
2025-10-01 09:25:33 +0300

    Update discord-alert.sh

--- laboratory/discord-alert.sh
+++ laboratory/discord-alert.sh
@@ -10,26 +10,26 @@ awk '{print 100 - $1}')
 RAM_USAGE=$(free | grep Mem | \
 awk '{printf("%.0f", ($3/$2) * 100.0)}')
 
-df -h | awk '$6 == "/" {
- gsub(/%/, "", $5)
- print $6 ": " $5 "%"
-}'
+#df -h | awk '$6 == "/" {
+# gsub(/%/, "", $5)
+# print $6 ": " $5 "%"
+#}'
 
-if (( CPU_USAGE > THRESHOLD )); then
- MESSAGE="⚠ Disk Alert! Root partition '/' usage is at ${USAGE}% (threshold: ${THRESHOLD}%)"
-curl -H "Content-Type: application/json" \
- -X POST \
- -d "{\"content\": \"$MESSAGE\"}" \
- "$WEBHOOK_URL"
-fi
+#if (( CPU_USAGE > THRESHOLD )); then
+# MESSAGE="⚠ Disk Alert! Root partition '/' usage is at ${USAGE}% (threshold: ${THRESHOLD}%)"
+#curl -H "Content-Type: application/json" \
+# -X POST \
+# -d "{\"content\": \"$MESSAGE\"}" \
+# "$WEBHOOK_URL"
+#fi
 
-if (( RAM_USAGE > THRESHOLD )); then
- MESSAGE="⚠ Disk Alert! Root partition '/' usage is at ${USAGE}% (threshold: ${THRESHOLD}%)"
-curl -H "Content-Type: application/json" \
- -X POST \
- -d "{\"content\": \"$MESSAGE\"}" \
- "$WEBHOOK_URL"
-fi
+#if (( RAM_USAGE > THRESHOLD )); then
+# MESSAGE="⚠ Disk Alert! Root partition '/' usage is at ${USAGE}% (threshold: ${THRESHOLD}%)"
+#curl -H "Content-Type: application/json" \
+# -X POST \
+# -d "{\"content\": \"$MESSAGE\"}" \
+# "$WEBHOOK_URL"
+#fi
 
 if (( USAGE > THRESHOLD )); then
  MESSAGE="⚠ Disk Alert! Root partition '/' usage is at ${USAGE}% (threshold: ${THRESHOLD}%)"
